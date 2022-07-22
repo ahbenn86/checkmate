bad72613f05112ed90763df167010b3c15244477
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-07-22 19:17:21 -0400

    Added  nav script | minor html & css

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+//Pages
+document.querySelectorAll('.logo').forEach(logo => {
+    logo.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = ''
+        document.querySelector('.login-page').style.display = 'none'
+        document.querySelector('.signup-page').style.display = 'none'
+    })
+})
+document.querySelectorAll('.login').forEach(loginBtn => {
+    loginBtn.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'none'
+        document.querySelector('.login-page').style.display = 'block'
+        document.querySelector('.signup-page').style.display = 'none'
+    })
+})
+document.querySelectorAll('.signup').forEach(signupBtn => {
+    signupBtn.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = 'none'
+        document.querySelector('.login-page').style.display = 'none'
+        document.querySelector('.signup-page').style.display = 'flex'
+    })
+})
+document.querySelectorAll('.continue-btn').forEach(contBtn => {
+    contBtn.addEventListener('click', () => {
+        document.querySelector('.front-page').style.display = ''
+        document.querySelector('.login-page').style.display = 'none'
+        document.querySelector('.signup-page').style.display = 'none'
+    })
+})
+//End of pages
+
 //Navigation
 const dropdownItems = document.querySelectorAll('.dropdown-hover')
 
